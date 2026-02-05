70eaf992a7f839a43a5a1a339f97b1f25fcab23d
Wajid Jamadar <wajidjamadar@xx.local>
2026-02-05 10:42:53 +0530

    Update database config for deployment

--- app/database.py
+++ app/database.py
@@ -1,13 +1,14 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_9aspU0mOAWti@ep-wild-pond-aiwpw1hj-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+DATABASE_URL = config('DATABASE_URL', default='sqlite:///./inventory.db')
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+if DATABASE_URL.startswith('sqlite'):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
